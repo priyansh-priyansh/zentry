18bebe0551895957f0d6a2e9f42c6952f18016b7
priyansh-priyansh <red.priyansh@gmail.com>
2025-02-08 11:26:03 +0530

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {FaDiscord, FaGithub, FaTwitter , FaTwitch} from "react-icons/fa";
 const links = [
   {href: 'https://discord.com' , icon: <FaDiscord/> },
   {href: 'https://twitter.com' , icon: <FaTwitter/> },
-  {href: 'https://github.com' , icon: <FaGithub/> },
+  {href: 'https://github.com/priyansh-priyansh' , icon: <FaGithub/> },
   {href: 'https://twitch.com' , icon: <FaTwitch/> },
 ]
 
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className="w-screen bg-violet-300 py-4 text-black">
     <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm md:text-left">
-          &copy; Nova 2024. All rights reserved
+          &copy; Priyansh 2025. All rights reserved
         </p>
 
       <div className="flex justify-center gap-4 md:justify-start">
